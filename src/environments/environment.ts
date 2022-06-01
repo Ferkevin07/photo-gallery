d2de19d05917a5057e74bc7568e131fff1ad91a6
Ferkevin07 <ferkevin71997@hotmail.com>
2022-06-01 15:42:51 -0500

    Finiched

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'a-216a9',
+    appId: '1:85821775228:web:fa2815b61a186abf5918f9',
+    storageBucket: 'a-216a9.appspot.com',
+    apiKey: 'AIzaSyDNnQSC9rpGH7Cb9Kd7QX2k_6QdPozjrCc',
+    authDomain: 'a-216a9.firebaseapp.com',
+    messagingSenderId: '85821775228',
+    measurementId: 'G-QKM1VL2ZR2',
+  },
   production: false
 };
 
